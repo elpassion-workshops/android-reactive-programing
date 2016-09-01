b9a0b4973e7e87a18abde61912d5829cd55db02d
Karol Kowalski <ordon1313@gmail.com>
2016-09-01 23:18:43 +0200

    Change to calculating log10 twice but separately

--- src/main/java/com/elpassion/whyrx/Observable.kt
+++ src/main/java/com/elpassion/whyrx/Observable.kt
@@ -9,4 +9,8 @@ class Observable<T>(val executor: Executor, val provider: Provider<T>) {
             onSuccess.call(provider.provide())
         }
     }
+
+    fun <R> flatMap(function: (T) -> R): Observable<R> {
+        return Observable(executor, Provider { function.invoke(provider.provide()) })
+    }
 }
--- src/main/java/com/elpassion/whyrx/ReactiveExample.kt
+++ src/main/java/com/elpassion/whyrx/ReactiveExample.kt
@@ -5,8 +5,7 @@ import java.util.concurrent.Executor
 class ReactiveExample(val executor: Executor) {
 
     fun calculate(input: Double): Observable<Double> {
-        return Observable<Double>(executor, Provider {
-            log10(log10(input))
-        })
+        return Observable(executor, Provider{ log10(input) })
+                .flatMap({ log10(it) })
     }
 }
