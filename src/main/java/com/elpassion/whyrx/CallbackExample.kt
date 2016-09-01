80520ebcbb3e49634c6fb58d8fdc0c474e6da6de
Karol Kowalski <ordon1313@gmail.com>
2016-09-01 21:35:41 +0200

    Extract log10 as a long running operation

--- src/main/java/com/elpassion/whyrx/CallbackExample.kt
+++ src/main/java/com/elpassion/whyrx/CallbackExample.kt
@@ -5,8 +5,14 @@ import java.util.concurrent.Executor
 class CallbackExample(val executor: Executor) {
 
     fun calculate(input: Double, resultCallback: Callback<Double>) {
+        calculateLog10(input, Callback {
+            calculateLog10(it, resultCallback)
+        })
+    }
+
+    fun calculateLog10(input: Double, resultCallback: Callback<Double>) {
         executor.execute {
-            resultCallback.call(Math.log10(Math.log10(input)))
+            resultCallback.call(log10(input))
         }
     }
 }
---
+++ src/main/java/com/elpassion/whyrx/Log10.kt
@@ -0,0 +1,6 @@
+package com.elpassion.whyrx
+
+fun log10(input: Double): Double {
+    Thread.sleep(100)
+    return Math.log10(input)
+}
