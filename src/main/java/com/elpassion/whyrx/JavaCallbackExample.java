a128a83b0d9c455d7cc9891ef353ed5fd3ce1f3b
Karol Kowalski <ordon1313@gmail.com>
2016-09-02 00:03:54 +0200

    Add JavaCallbackExample

---
+++ src/main/java/com/elpassion/whyrx/JavaCallbackExample.java
@@ -0,0 +1,39 @@
+package com.elpassion.whyrx;
+
+import java.util.concurrent.Executor;
+
+public class JavaCallbackExample {
+    private final Executor executor;
+
+    public JavaCallbackExample(Executor executor) {
+        this.executor = executor;
+    }
+
+    public void calculate(final Double input, final Callback<Double> onSuccess, final Callback<Exception> onError) {
+        calculateLog10(input, new Callback<Double>() {
+            public void call(Double result) {
+                calculateLog10(result, onSuccess, onError);
+            }
+        }, onError);
+    }
+
+    private void calculateLog10(final Double input, final Callback<Double> onSuccess, final Callback<Exception> onError) {
+        executor.execute(new Runnable() {
+            public void run() {
+                try {
+                    onSuccess.call(log10(input));
+                } catch (Exception e) {
+                    onError.call(e);
+                }
+            }
+        });
+    }
+
+    private Double log10(Double input) {
+        double log10 = Math.log10(input);
+        if (Double.isNaN(log10)) {
+            throw new IllegalArgumentException();
+        }
+        return log10;
+    }
+}
