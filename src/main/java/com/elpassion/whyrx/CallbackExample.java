93598b6b50b47c69143d492f1a42cb82a9b16918
Karol Kowalski <ordon1313@gmail.com>
2016-09-06 13:39:13 +0200

    Add secend case to force calculation using log10 twice

--- src/main/java/com/elpassion/whyrx/CallbackExample.java
+++ src/main/java/com/elpassion/whyrx/CallbackExample.java
@@ -3,7 +3,10 @@ package com.elpassion.whyrx;
 public class CallbackExample {
 
     Double calculate(Double input) {
-        return input;
+        return log10(log10(input));
     }
 
+    Double log10(Double input){
+        return Math.log10(input);
+    }
 }
--- src/test/java/com/elpassion/whyrx/CallbackExampleTest.kt
+++ src/test/java/com/elpassion/whyrx/CallbackExampleTest.kt
@@ -8,7 +8,12 @@ class CallbackExampleTest {
     val calculator = CallbackExample()
 
     @Test
-    fun shouldReturn1ForInputEquals1() {
-        Assert.assertEquals(1.0, calculator.calculate(1.0), Double.MIN_VALUE)
+    fun shouldReturn0ForInputEquals10() {
+        Assert.assertEquals(0.0, calculator.calculate(10.0), Double.MIN_VALUE)
+    }
+
+    @Test
+    fun shouldReturn1ForInputEquals1AndTen0s() {
+        Assert.assertEquals(1.0, calculator.calculate(10000000000.0), Double.MIN_VALUE)
     }
 }
