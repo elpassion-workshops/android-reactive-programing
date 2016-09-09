b060fe58ef94a3b4bb96bda8633a62df321efb0c
Karol Kowalski <ordon1313@gmail.com>
2016-09-09 16:21:04 +0200

    Callback start

--- src/test/java/com/elpassion/whyrx/SumOfPrimeSquaredCalculatorTest.kt
+++ src/test/java/com/elpassion/whyrx/SumOfPrimeSquaredCalculatorTest.kt
@@ -1,15 +1,25 @@
 package com.elpassion.whyrx
 
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.verify
 import org.junit.Assert
 import org.junit.Test
 
 class SumOfPrimeSquaredCalculatorTest {
 
     private val calculator = Calculator()
-    private val sumOfPrimeSquaredCalculator = SumOfPrimeSquaredCalculator(calculator)
+    private val callbackCalculator = CallbackCalculator(calculator)
+    private val sumOfPrimeSquaredCalculator = SumOfPrimeSquaredCalculator(calculator, callbackCalculator)
 
     @Test
     fun shouldCalculateSumOfPrimeSquaredSynchronously() {
         Assert.assertEquals(1 + 4 + 9 + 25, sumOfPrimeSquaredCalculator.calculateSynchronously(7))
+    }
+
+    @Test
+    fun shouldCalculateSumOfPrimeSquaredWithCallbkack() {
+        val onSuccess = mock<Callback<Int>>()
+        sumOfPrimeSquaredCalculator.calculateWithCallback(7, onSuccess)
+        verify(onSuccess).call(1 + 4 + 9 + 25)
     }
 }
